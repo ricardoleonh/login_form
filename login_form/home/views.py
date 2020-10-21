8d7beecdf3ba4644b3dc1f54992d5d290299903d
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-10-20 20:09:26 -0500

    added render views

--- login_form/home/views.py
+++ login_form/home/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'login.html')
+
+def signin(request):
+    return render(request, 'dashboard.html')
+
+def register(request):
+    return render(request, 'registration.html')
+
+def login(request):
+    return render(request, 'login.html')
