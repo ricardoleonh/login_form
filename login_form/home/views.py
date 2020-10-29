83478363208586313d75479e750c601f5ffd70dc
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-10-28 23:37:34 -0500

    working on the registration view

--- login_form/home/views.py
+++ login_form/home/views.py
@@ -7,10 +7,45 @@ def index(request):
     return render(request, 'login.html')
 
 def signin(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'login.html')
 
-def register(request):
+def register_form(request):
     return render(request, 'registration.html')
 
+def dashboard(request):
+    if 'user_id' not in request.session: #validate user is logged in
+        return redirect('/')
+    context = {
+        'user' : User.objects.get(id=request.session['user_id'])
+    }
+    return render (request, 'dashboard.html', context)
+
+def registration(request):
+    if request.method == 'POST':
+        errors = User.objects.reg_validator(request.POST) #to validate the form is completed correctly
+        if len(errors) != 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect ('/register')
+        hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode() #to hash the password
+        new_user = User.objects.create ( #to create a new user
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['mail'],
+            password = hashed_pw
+        )
+        request.session['user_first_name'] = new_user.first_name
+        request.session['user_last_name'] = new_user.last_name
+        request.session['user_mail'] = new_user.mail
+        request.session['user_id'] = new_user.id
+        return redirect ('/dashboard')
+    return redirect ('/')
+
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        errors = User.objects.log_validator(request.POST) #to validate the form is completed correctly
+        if len(errors) != 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect ('/')
+        
